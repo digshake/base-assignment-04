b884b8ba62a162f80772615d5c227772a7d5dab3
Dennis Cosgrove <cosgroved@wustl.edu>
2021-02-11 16:55:37 -0600

    spring 2021 changes

---
+++ src/assignment4/ZombieData.java
@@ -0,0 +1,26 @@
+package assignment4;
+
+import java.awt.Color;
+import java.util.Objects;
+
+import edu.princeton.cs.introcs.StdDraw;
+import support.cse131.ArgsProcessor;
+import zombies.ZombieSimulationFiles;
+
+public class ZombieData {
+
+	public static void main(String[] args) throws Exception {
+		ArgsProcessor ap = ZombieSimulationFiles.createArgsProcessorFromFile(args);
+
+		// TODO: 1. Select the file you which to read from the file dialog box.
+
+		// TODO: 2. Read in the number of entities from the ArgsProcessor
+
+		// TODO: 3. Create the arrays that will hold entity data
+
+		// TODO: 4. Read in all the Entity data
+
+		// TODO: 5. Iterate through all the data and display it using StdDraw
+
+	}
+}
