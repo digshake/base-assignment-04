e835c0c6444a88123ef25315cf773fdd797ff72e
Dennis Cosgrove <cosgroved@wustl.edu>
2021-02-18 16:10:51 -0600

    comment update

--- src/assignment4/ZombieData.java
+++ src/assignment4/ZombieData.java
@@ -9,17 +9,17 @@ import zombies.ZombieSimulationFiles;
 public class ZombieData {
 
 	public static void main(String[] args) throws Exception {
+		// NOTE: The line below will prompt the user with a file open dialog box.
+		//       The contents of the selected file will be made available via the ArgsProcessor ap.
 		ArgsProcessor ap = ZombieSimulationFiles.createArgsProcessorFromFile(args);
 
-		// TODO: 1. Select the file you wish to read from the file dialog box.
+		// TODO: 1. Read in the number of entities from the ArgsProcessor
 
-		// TODO: 2. Read in the number of entities from the ArgsProcessor
+		// TODO: 2. Create the arrays that will hold entity data
 
-		// TODO: 3. Create the arrays that will hold entity data
+		// TODO: 3. Read in all the Entity data
 
-		// TODO: 4. Read in all the Entity data
-
-		// TODO: 5. Iterate through all the data and display it using StdDraw
+		// TODO: 4. Iterate through all the data and display it using StdDraw
 
 	}
 }
