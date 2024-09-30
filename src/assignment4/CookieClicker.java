7ee0ada792cbbb7428a00e9d341a83c2b61cecc1
dssho <dssho@DESKTOP-2ASM3QT.dhcp.wustl.edu>
2024-09-30 12:19:52 -0500

    Cookies!

---
+++ src/assignment4/CookieClicker.java
@@ -0,0 +1,16 @@
+package assignment4;
+
+import java.awt.Color;
+import java.io.File;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
+
+import edu.princeton.cs.introcs.StdDraw;
+
+public class CookieClicker {
+
+	public static void main(String[] args) {
+		
+	}
+}
